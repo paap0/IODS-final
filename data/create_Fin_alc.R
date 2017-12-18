6e5ac38cfd1305fec35b0b493d0b9f55e1a0d4ad
paap0 <33256735+paap0@users.noreply.github.com>
2017-12-18 13:25:25 +0200

    Wrangling

---
+++ data/create_Fin_alc.R
@@ -0,0 +1,145 @@
+#Paula paap0 Bergman Dec 2017 Final assignment
+#Final assignment: Data wrangling
+#The orginal dataset from F. Pagnotta & H. M. Amran (2008). Using Data Mining To Predict Secondary
+#School Student Alcohol Consumption. Department of Computer Science,
+#University of Camerino. Referred 05/02/17.
+#https://archive.ics.uci.edu/ml/datasets/STUDENT+ALCOHOL+CONSUMPTION
+
+####################################
+####################################
+##FINAL ASSIGNMENT DATA WRANGLING ##
+####################################
+####################################
+
+library(dplyr)
+#zip-file downloaded, files unzipped, the two files ectracted and moved in my folder
+
+#New script created. Name, date and short description written
+
+#Working directory set
+setwd("~/GitHub/IODS-final/data")
+
+#Data read
+math <- as.data.frame(read.table('student-mat.csv', sep=';', header = TRUE))
+portugese <- as.data.frame(read.table('student-por.csv', sep=';', header = TRUE))
+
+#Structure and dimensions explored with glimpse() (even though already done week3)
+glimpse(math)#395 observations and 33 variables
+glimpse(portugese)#649 observations and 33 variables
+
+#Identifiers to be used in joining the tables according to my preliminary interest
+join_by<-c("sex","age","Pstatus","Medu","Fedu","Mjob","Fjob","reason","nursery")
+
+#Join the datasets based on the identifier columns
+mathpor <- inner_join(math, portugese, by = join_by, suffix = c(".math", ".portugese"))
+
+#Explore the structure and dimensions
+glimpse(mathpor)#434 observations and 57 variables
+#4.Merging results in numerous duplicated variables as can be seen
+colnames<-colnames(mathpor)
+colnames
+
+#5.Create a data frame with only the joined columns
+alc<-dplyr::select(mathpor,one_of(join_by))
+glimpse(alc)#382 observations and 13 variables
+
+#5.To combine the "duplicated" answers firstly define the ones not used for joining
+nonid_col<-colnames(math)[!colnames(math) %in% join_by]
+#5.Let's look at their names
+nonid_col
+
+#5.By for looping select two collumns from mathpor with the same name and
+#if the first is numeric calculate a rounded average of the two
+#if it in not numeric, then include the first of the two 
+
+for(colnames in nonid_col) {
+    two_columns<-dplyr::select(mathpor,starts_with(colnames))
+    first_column<-dplyr::select(two_columns,1)[[1]]
+    
+    if(is.numeric(first_column)){
+       alc[colnames]<-round(rowMeans(two_columns))      
+    }  
+       else {
+       alc[colnames]<-first_column
+      }
+}
+
+#5.Checking the structure of the generated dataset
+glimpse(alc) #434 observations 33 variables
+
+write.csv(alc, file = "alc.csv", row.names = FALSE)
+
+alc<-read.csv(file="alc.csv", header=TRUE)
+
+
+#6.Create variable alc_use by averaging weekdays and weekends consuption
+alc <- mutate(alc, alc_useall = (Dalc + Walc) / 2)
+
+#Further, create variables for weekday and weekend use, I want to rename variables
+#to better keep track on what I am doing
+library(plyr)                    
+
+alc$alcALLsuht <- as.factor(ifelse(((alc$Dalc * 5 + alc$Walc* 2) / 7) < 3, 0, 1))
+alc$alclotW_or_WE<-as.factor(ifelse(alc$Dalc>2|alc$Walc>2,1,0))
+
+str(alc)
+
+colnames(alc)
+
+summary(alc$age)
+alc$agecat<-cut(alc$age,breaks=c(0,16,17,Inf))
+alc$Meducat<-cut(alc$Medu, breaks=c(-1,2,3,4))
+alc$Feducat<-cut(alc$Fedu, breaks=c(-1,2,3,4))
+alc$Famrelcat<-cut(alc$famrel, breaks=c(0,2,3,5))
+alc$gooutcat<-cut(alc$goout, breaks=c(0,2,3,5))
+alc$alcweekcat<-cut(alc$Dalc, breaks=c(0,2,5))
+alc$alcendcat<-cut(alc$Walc, breaks=c(0,2,5))
+alc$healthcat<-cut(alc$health, breaks=c(0,2,3,5))
+alc$failurescat<-as.factor(ifelse(alc$failures>0,1,0))
+alc$absencescat<-cut(alc$absences,breaks=c(-Inf,1,6,Inf))
+alc$G3cat<-cut(alc$G3,breaks=c(-Inf,10,12,14,Inf))
+
+
+dfalc<-alc[c("sex","agecat","Pstatus","Meducat","Feducat","Mjob","Fjob","guardian","famsup",
+                       "higher","romantic","activities","Famrelcat","gooutcat",
+                       "healthcat","failurescat","absencescat", "G3cat","G3","alclotW_or_WE")]
+
+dfalc<-plyr::rename(dfalc, c("sex"="Gender",
+                          "agecat"="Agegroup",
+                          "Pstatus"="Parents together",
+                          "Meducat"="Education_M",
+                          "Feducat"="Education_F",
+                          "Mjob"="Job_M",
+                          "Fjob"="Job_F",
+                          "guardian"="Guardian",
+                          "famsup"="Familial_support",
+                          "higher"="Education_pos",
+                          "romantic"="In_relationship",
+                          "activities"="Activities",
+                          "Famrelcat"="Family_relationships",
+                          "gooutcat"="Going_out",
+                          "healthcat"="Healthgroup",
+                          "failurescat"="Class_failures",
+                          "absencescat"="Absencegroup", 
+                          "G3cat"="Performancegroup",
+                          "G3"="Final_Grade",
+                          "alclotW_or_WE"="High_alcohol"))
+
+
+dfalc<-mutate_all(dfalc,as.factor)
+dfalc$Final_Grade<-as.numeric(dfalc$Final_Grade)
+
+
+str(dfalc)
+table(is.na(dfalc))
+
+#7. Save and check the created, joined and modified dataset 
+write.csv(dfalc, file = "alccatfiso.csv", row.names = FALSE)
+
+alctest<-read.csv(file="alccatfiso.csv", header=TRUE)
+dim(alctest)#should be 434 obs and 20 variables: Correct!
+str(alctest)
+
+
+
+
